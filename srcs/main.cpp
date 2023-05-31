a73873411c0b79e8976bd583aaaaa1c5ee7dcfec
Juyoung Ha <juha@c9r3s1.42seoul.kr>
2023-05-31 20:44:04 +0900

    [FIX] check port number
    strtol을 사용하여 에러를 검출했습니다.
    strtol은 숫자가 아닌 값이 나오면 데이터를 저장합니다.
    해당값이 '\0'일 경우 숫자가 아닌 값이 들어오지 않았음을 알 수 있습니다.
    
    ex) 1.2를 strtol으로 계산할 경우
    	1은 값에 담기겠지만 나머지 .과 2는 담 isError에 담기게 됩니다.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,13 +5,11 @@ int main(int argc, char **argv)
     if (argc != 3)
         ErrorHandle::errorHandle("Usage : ./ircserv [port] [password]", 0);
     (void)argc;
-    std::strstream sstream;
-    int port;
 
-    // port 번호 확인 65535 까지인지 확인,
-    sstream << argv[1];
-    sstream >> port;
-    if (sstream.fail())
+    char *isError = NULL;
+    long port = std::strtol(argv[1], &isError, 10);
+
+    if (*isError || (0 > port && 65535 < port))
         ErrorHandle::errorHandle("port error", -1);
 
     IrcServ serv(port, argv[2]);
