1e87aee52a1590335824859a2f2b7744edd3b6cb
Jihye Wee <jwee@c9r4s3.42seoul.kr>
2023-06-08 23:16:07 +0900

    [refac] IrcChannel 구현,  IrcCommand, IrcChannel에서 interface 제거

--- include/IrcChannel.hpp
+++ include/IrcChannel.hpp
@@ -1,14 +1,9 @@
-#ifndef __IrcCHANNEL__
-#define __IrcCHANNEL__
+#ifndef __IRC_CHANNEL_HPP__
+#define __IRC_CHANNEL_HPP__
 
 #include "../InterfaceClass/IChannel.hpp"
 
-// class IrcServ;
-class IrcClient;
-
-#define MAX_CLIENT_BUFFER_SIZE 1024
-
-class IrcChannel : private IChannel
+class IrcChannel
 {
 public:
     IrcChannel();
@@ -22,14 +17,24 @@ public:
     virtual void setOperator(const int clientFd, const int target);
     virtual void setTopic(std::string& newTopic);
     virtual void setPassword(std::string& newPassword);
+    virtual const bool isOperator(const int clientFd);
 
     virtual const int getGrant() const;
-    virtual const bool isOperator(const int clientFd) const;
+
     virtual const std::string& getTopic() const;
     virtual const std::string& getName() const;
     virtual const std::string& getPassword() const;
 
-protected:
+private:
+    const IrcChannel& operator=(const IrcChannel& copy);
+    IrcChannel(const IrcChannel& copy);
+    class InvalidGrant {
+        public:
+            virtual const char *what() const throw();
+    };
+    class InvalidClientFd : public std::exception {
+        virtual const char *what() const throw() { return "invalidClientFd"; }
+    };
     std::string _topic;
     std::string _name;
     std::string _password;
@@ -37,11 +42,6 @@ protected:
     int         _grant;
 
     std::map<int, bool> _user;
-
-private:
-    const IrcChannel& operator=(const IrcChannel& copy);
-    IrcChannel(const IrcChannel& copy);
-    IrcChannel();
 };
 
 #endif
--- srcs/IrcChannel.cpp
+++ srcs/IrcChannel.cpp
@@ -1,20 +1,30 @@
-// #include <arpa/inet.h>
-// #include "../include/Irc.hpp"
-
 #include "../include/IrcChannel.hpp"
 
-IrcChannel::IrcChannel() 
-    : _name("no Name"), _topic("any thing") {}
-
+IrcChannel::IrcChannel() : _name("no Name"), _topic("any thing") {}
+ 
 IrcChannel::IrcChannel(std::string name)
     : _name(name), _topic("any thing") {}
 
 IrcChannel::~IrcChannel() {}
 
-
-const bool IrcChannel::addUser(const int clientFd) {}
-const bool IrcChannel::deleteUser(const int clientFd, const int target) {}
-const bool IrcChannel::isJoinedUser(const int clientFd) {}
+//addUser시 이미 존재하는 user는 add하지 않음
+const bool IrcChannel::addUser(const int clientFd) {
+    if (isJoinedUser(clientFd)){
+	    _user[clientFd] = false;
+        return true;
+    }
+    return false;
+}
+const bool IrcChannel::deleteUser(const int clientFd, const int target) {
+    if (isOperator(clientFd)){
+        _user.erase(target);
+        return true;
+    }
+    return false;
+}
+const bool IrcChannel::isJoinedUser(const int clientFd) {
+    return _user.count(clientFd) != 0;
+}
 
 void IrcChannel::setGrant(typeMode grant, bool on) {
     if (on){
@@ -25,16 +35,20 @@ void IrcChannel::setGrant(typeMode grant, bool on) {
         _grant &= ~grant;
 }
 
-void IrcChannel::setOperator(const int clientFd, const int target) {}
+void IrcChannel::setOperator(const int clientFd, const int target) {
+    if (isOperator(clientFd))
+        _user[target] = true;
+}
 void IrcChannel::setTopic(std::string& newTopic) { _topic = newTopic; }
 void IrcChannel::setPassword(std::string& newPassword) { _password = newPassword; }
 
 const int IrcChannel::getGrant() const { return _grant; }
-const bool IrcChannel::isOperator(const int clientFd) const {
-    try {
-        int result;
-        resutl = _user.find(clientFd);
+const bool IrcChannel::isOperator(const int clientFd){
+    if (isJoinedUser(clientFd))
+        return static_cast<const bool>(_user[clientFd]);
+    throw InvalidClientFd();
 }
-const std::string& IrcChannel::getTopic() const {}
-const std::string& IrcChannel::getName() const {}
-const std::string& IrcChannel::getPassword() const {}
+
+const std::string& IrcChannel::getTopic() const { return _topic; }
+const std::string& IrcChannel::getName() const { return _name; }
+const std::string& IrcChannel::getPassword() const { return _password; }
