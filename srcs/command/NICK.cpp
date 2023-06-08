f3388e2863dcb981466e23a2c25b5821f231dab8
Jihye Wee <jwee@c9r4s3.42seoul.kr>
2023-06-08 18:44:51 +0900

    [refact] 충돌 방지

--- srcs/command/NICK.cpp
+++ srcs/command/NICK.cpp
@@ -1,22 +1,10 @@
 #include "../../include/IrcCommand.hpp"
 
-<<<<<<< HEAD
-NICK::NICK(){}
-NICK::~NICK(){}
-
-void NICK::execute(vector<std::string> args){
-
-	if (args.size() != 1)
-		throw std::exception(); //
-	_nickname = args[0];
-}
-=======
 
 //맞춰서 추가해야 함. jwee가 수정할거임???
 
 NICK::NICK(){}
 NICK::~NICK(){}
->>>>>>> 37ddfd7eb53da24f168babb4e1f4ca82c1175692
 
 void execute(Vector<std::string> args, Client& target){
 	target.setNickname(args[0]);
