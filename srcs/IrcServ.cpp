c4e681df5b1ab3561694a1e209299f8183181f62
juha <contemplation.person@gmail.com>
2023-06-09 01:53:17 +0900

    [ADD] exception
    내일 논의 해봐야 하는 사항.
    * 처음에 메시지를 날려서 추가를 해야함.
    * 생성자가 생성이 안됨.

--- srcs/IrcServ.cpp
+++ srcs/IrcServ.cpp
@@ -1,18 +1,11 @@
-#include "../include/Irc.hpp"
-
+#include "../include/IrcServ.hpp"
 #include <fcntl.h>
 #include <arpa/inet.h>
 #include <cstring>
 #include <unistd.h>
 
 IrcServ::IrcServ(int port, std::string passWord)
-    : _isError(0)
-    , _port(port)
-    , _passWord(passWord)
-    , _servFd(0)
-    , _fdMax(3)
-    , _fdNum(0)
-    , _opt(1)
+    : _isError(0), _port(port), _passWord(passWord), _servFd(0), _fdMax(3), _fdNum(0), _opt(1)
 {
     std::memset(&_servAddr, 0, sizeof(_servAddr));
     _servAddr.sin_family = AF_INET;
@@ -21,49 +14,54 @@ IrcServ::IrcServ(int port, std::string passWord)
     _servFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     if (_servFd == -1)
-        Function::errorHandle("socket error", _servFd);
+        throw IrcServ::socketException();
+
+    _isError = fcntl(_servFd, O_NONBLOCK);
+    if (_isError == -1)
+        throw IrcServ::fcntlException();
 
     _isError = setsockopt(_servFd, SOL_SOCKET, SO_REUSEADDR, &_opt, static_cast<socklen_t>(sizeof(_opt)));
     if (_isError)
-        Function::errorHandle("setsockopt error", _isError);
+        throw IrcServ::setsockoptException();
 
-    _isError = bind(_servFd, (struct sockaddr*)&_servAddr, sizeof(_servAddr));
+    _isError = bind(_servFd, (struct sockaddr *)&_servAddr, sizeof(_servAddr));
     if (_isError)
-        Function::errorHandle("fail bind", _isError);
+        throw IrcServ::bindException();
 
-    _isError = listen(_servFd, 5);// 이해  필요.
+    _isError = listen(_servFd, 5); // 이해  필요.
     if (_isError)
-        Function::errorHandle("fail listen", _isError);
+        throw IrcServ::listenException();
 }
 
 void IrcServ::run()
 {
+    // IrcCommand command(); // 왜 안됨?
     FD_ZERO(&_activeReads);
     FD_ZERO(&_activeWrites);
     FD_SET(_servFd, &_activeReads);
 
-    struct timeval  timeout;
-    int             acceptFd;
+    struct timeval timeout;
+    int acceptFd;
 
     _fdMax = _servFd;
 
-    while(true) 
+    while (true)
     {
-        FD_COPY(&_activeReads, &_cpyReads);
-        FD_COPY(&_activeWrites, &_cpyWrites);
-        // _cpyReads = _activeReads;
-        // _cpyWrites = _activeWrites;
+        // FD_COPY(&_activeReads, &_cpyReads);
+        // FD_COPY(&_activeWrites, &_cpyWrites);
+        _cpyReads = _activeReads;
+        _cpyWrites = _activeWrites;
         timeout.tv_sec = 0;
         timeout.tv_usec = 100;
 
         _fdNum = select(_fdMax + 1, &_cpyReads, &_cpyWrites, 0, &timeout);
         if (_fdNum == -1)
-            Function::errorHandle("select error", -1);
+            throw IrcServ::selectException();
 
-        char               message[BUFFER_SIZE];
-        int                readLen;
+        char message[BUFFER_SIZE];
+        int readLen;
         struct sockaddr_in clientAddr;
-        socklen_t          clientAddrLen;
+        socklen_t clientAddrLen;
 
         std::memset(message, 0, sizeof(message));
 
@@ -79,11 +77,11 @@ void IrcServ::run()
                     std::memset(&clientAddr, 0, sizeof(clientAddr));
                     clientAddrLen = sizeof(clientAddr);
                     acceptFd = accept(_servFd, (struct sockaddr *)&clientAddr, &clientAddrLen);
-                    if (acceptFd == -1)
+                    if (acceptFd == -1 || fcntl(acceptFd, O_NONBLOCK) == -1)
                         continue;
-                    if (fcntl(acceptFd, O_NONBLOCK) == -1)
-                        Function::errorHandle("fcntl", -1);
-                    _clients[acceptFd] = IrcClient(acceptFd, clientAddr, clientAddrLen);
+                    // getclient를 해야함.
+                    // command.setCommand("PASS");
+                    // _clients[acceptFd] = IrcClient(acceptFd, clientAddr, clientAddrLen);
                     FD_SET(acceptFd, &_activeReads);
                     FD_SET(acceptFd, &_activeWrites);
                     if (_fdMax < acceptFd)
@@ -97,11 +95,11 @@ void IrcServ::run()
                         FD_CLR(i, &_activeReads);
                         FD_CLR(i, &_activeWrites);
                         close(i);
-                        _clients.erase(_clients.find(i));
+                        //;delete _client를 해야함.
                     }
                     else
                     {
-                        std::cout << message << std::endl; // char??? std::string??? 
+                        std::cout << message << std::endl; // char??? std::string???
                     }
                 }
             }
@@ -109,6 +107,41 @@ void IrcServ::run()
         // std::cout << std::endl;
     }
     close(_servFd);
+}
+
+const char *IrcServ::socketException::what() const throw()
+{
+    return "socket error";
+}
+
+const char *IrcServ::fcntlException::what() const throw()
+{
+    return "fcntl error";
+}
+
+const char *IrcServ::setsockoptException::what() const throw()
+{
+    return "setsockopt error";
+}
+
+const char *IrcServ::bindException::what() const throw()
+{
+    return "bind error";
+}
+
+const char *IrcServ::listenException::what() const throw()
+{
+    return "listen error";
+}
+
+const char *IrcServ::selectException::what() const throw()
+{
+    return "select error";
+}
+
+const char *IrcServ::acceptException::what() const throw()
+{
+    return "accept error";
 }
 
 IrcServ::~IrcServ(){};
