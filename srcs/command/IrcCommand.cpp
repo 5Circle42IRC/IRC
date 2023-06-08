d8611d055f0d40b55ffe803a07eb3632bcba8029
Juyoung Ha <juha@c9r4s5.42seoul.kr>
2023-06-08 17:38:43 +0900

    [WIP] start Irc
    인터페이스 받아서 구현하자.

---
+++ srcs/command/IrcCommand.cpp
@@ -0,0 +1,10 @@
+#include "../include/IrcCommand.hpp"
+
+
+IrcCommand::IrcCommand(){}
+IrcCommand::~IrcCommand(){}
+
+void IrcCommand::foreach (T){}
+
+const vector<std::string> &getArgs() const{}
+void setArgs(){}
