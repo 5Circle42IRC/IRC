d95f063a5976f6383ef258f09ae3ec8b7aaa12a8
Juyoung Ha <juha@c9r4s1.42seoul.kr>
2023-06-09 15:24:06 +0900

    check

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,38 +1,34 @@
-#include "../include/IrcCommand.hpp"
+#include "../include/IrcServ.hpp"
 
-
-int main(){
-    Client A;
-
-
-
-    
+static void errorHandle(std::string message, int exitCode)
+{
+    std::cout << message << std::endl;
+    exit(exitCode);
 }
 
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        errorHandle("Usage : ./ircserv [port] [password]", 0);
+    (void)argc;
 
-// int main(int argc, char **argv)
-// {
-//     if (argc != 3)
-//         Function::errorHandle("Usage : ./ircserv [port] [password]", 0);
-//     (void)argc;
+    char *isError = NULL;
+    long port = std::strtol(argv[1], &isError, 10);
 
-//     char *isError = NULL;
-//     long port = std::strtol(argv[1], &isError, 10);
+    if (*isError || (0 > port || 65535 < port))
+        errorHandle("port error", -1);
 
-//     if (*isError || (0 > port || 65535 < port))
-//         Function::errorHandle("port error", -1);
-
-//     try {
-//         IrcServ serv(port, argv[2]);
-//         serv.run();
-//     } catch (std::exception& e) {
-//         std::cout << e.what() << std::endl;
-//     } catch (...) {
-//         std::cout << "unkown exception" << std::endl;
-//     }
-// /*
-//     printf("port : %d\n", port);
-//     printf("ip : %d\n", htonl(INADDR_ANY));
-// */
-//     return 0;
-// }
+    try {
+        IrcServ serv(port, argv[2]);
+        serv.run();
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    } catch (...) {
+        std::cout << "unkown exception" << std::endl;
+    }
+/*
+    printf("port : %d\n", port);
+    printf("ip : %d\n", htonl(INADDR_ANY));
+*/
+    return 0;
+}
