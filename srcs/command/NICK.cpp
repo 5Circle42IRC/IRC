85638f9602740782982e26e9f7957a7855327207
ysungwon <hwoarang09@naver.com>
2023-06-09 19:28:14 +0900

    [WIP]NICK

--- srcs/command/NICK.cpp
+++ srcs/command/NICK.cpp
@@ -1,4 +1,28 @@
 #include "../../include/IrcCommand.hpp"
+#include "../../include/IrcDB.hpp"
+#include "../../include/IrcClient.hpp"
+#include <string>
+#include <vector>
 
-NICK::NICK(){}
-NICK::~NICK(){}
+
+//vector<std::string> jwee, hello, world
+
+void IrcCommand::NICK(int clientFd){
+    std::cout << "in <NICK> command" << std::endl;
+    //std::cout << "argus : <" << _args << ">" << std::endl;
+
+    if (_args.size() != 1)
+        return ;
+
+    std::string newNickname;
+    
+    
+    newNickname = *(_args.begin());
+    
+    
+    
+    IrcClient& client = find(clientFd);
+    client.setNickname(newNickname);
+
+    return ;
+}
