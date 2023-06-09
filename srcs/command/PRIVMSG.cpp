8772c011b8fe9dadebe9563920a24c356ade8916
ysungwon <hwoarang09@naver.com>
2023-06-09 16:53:39 +0900

    ircClient operator='

--- srcs/command/PRIVMSG.cpp
+++ srcs/command/PRIVMSG.cpp
@@ -1,10 +1,10 @@
-#include "../../IrcCommand.hpp"
+#include "../../include/IrcCommand.hpp"
 
 PRVM::PRVM(){}
 PRVM::~PRVM(){}
 
 
-vector<std::string> jwee, hello, world
+//vector<std::string> jwee, hello, world
 
 void PRVM::execute(vector<std::string> args){
 	recieveCLient = _client.find("")
