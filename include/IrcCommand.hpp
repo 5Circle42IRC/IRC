0dd5438bfdd73725cc0def297c0bc1b134fa64b7
Jihye Wee <jwee@c9r4s3.42seoul.kr>
2023-06-09 16:42:51 +0900

    [refac] 상속 protected 관계 정리

--- include/IrcCommand.hpp
+++ include/IrcCommand.hpp
@@ -9,16 +9,16 @@ class IrcCommand: protected IrcDB {
 		IrcCommand();
 		virtual ~IrcCommand();
 
-		virtual void	foreach(std::map<int, IrcClient&>& client) = 0;
-		virtual void	foreach(std::map<std::string, IrcChannel&> channel) = 0;
-		virtual void	execute(int ClientFd, std::vector<std::string>args) = 0;
+		virtual void	foreach(std::map<int, IrcClient&>& client);
+		virtual void	foreach(std::map<std::string, IrcChannel&> channel);
+		virtual void	execute(int ClientFd, std::vector<std::string>args);
 
 
 		virtual const	std::vector<std::string>& getArgs() const;
 
-	private:
+	protected:
 		std::vector<std::string>  _args;
 
 };
 
-#endif;
+#endif
