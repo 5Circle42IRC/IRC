a2b3735796509d084e99c84becbd5a5f0a511662
Juyoung Ha <juha@c5r3s6.42seoul.kr>
2023-06-16 17:25:54 +0900

    [FIX] check size len

--- srcs/command/MODE.cpp
+++ srcs/command/MODE.cpp
@@ -1,6 +1,5 @@
 #include "../../include/IrcCommand.hpp"
 #include "../../include/IrcClient.hpp"
-<<<<<<< HEAD
 #include "../../include/IrcDB.hpp"
 #include "../../include/IrcChannel.hpp"
 
@@ -131,33 +130,3 @@ void IrcCommand::MODE()
     */
     std::cout << "------MODE end------" << std::endl; 
 }
-=======
-
-static int checkValidMODE(std::deque<std::string>		args, IrcDB *_db)
-{
-    std::string nick = args[0]; 
-    if (nick.size() > 9)
-    {
-        std::cout << "NICK size have to be lower than 9" << std::endl;
-        throw std::exception();
-    }
-    if (_db->findClientByName(nick)->getNickname() == nick)
-    {
-        std::cout << "Already same nick exists." << std::endl;
-        std::cout << "nick <" << nick << ">  " << "ClientFd : <" << _db->findClientByName(nick)->getFd() << ">" << std::endl;
-        throw std::exception();        
-    } 
-    return 1;
-}
-
-void IrcCommand::MODE(int clientfd){
-    
-        if (getArgs().size() != 1)
-            std::cout << "NICK args size is not 1" << std::endl;
-        else
-        {
-            if (checkValidNICK(getArgs(), _db) == 1 )
-                _db->findClientByFd(clientfd)->setNickname(getArgs()[0]);
-        }    
-}
->>>>>>> 4148e28f0618c9aecb9214cb85b37f79067cc4a5
