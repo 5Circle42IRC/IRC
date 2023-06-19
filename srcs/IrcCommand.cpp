1d2e826e2eb4f6de763586d521210ad848e882ef
ysungwon <hwoarang09@naver.com>
2023-06-19 20:55:57 +0900

    hi

--- srcs/IrcCommand.cpp
+++ srcs/IrcCommand.cpp
@@ -6,76 +6,65 @@ IrcCommand::IrcCommand(IrcDB *db): _db(db) {
 	_commandList["JOIN"] = &IrcCommand::JOIN;
 	_commandList["NICK"] = &IrcCommand::NICK;
 	_commandList["PART"] = &IrcCommand::PART;
-	_commandList["PONG"] = &IrcCommand::PING;
+	_commandList["PONG"] = &IrcCommand::PONG;
 	_commandList["PRIVMSG"] = &IrcCommand::PRIVMSG;
 	_commandList["TOPIC"] = &IrcCommand::TOPIC;
 	_commandList["USER"] = &IrcCommand::USER;
 	_commandList["MODE"] = &IrcCommand::MODE;
 	_commandList["DISPLAY"] = &IrcCommand::DISPLAY;
-	_commandList["PASS"] = &IrcCommand::PASS;
-	_commandList["KICK"] = &IrcCommand::KICK;	
 	_commandNames.push_back("INVITE");
 	_commandNames.push_back("JOIN");
 	_commandNames.push_back("NICK");
 	_commandNames.push_back("PART");
-	_commandNames.push_back("PING");
+	_commandNames.push_back("PONG");
 	_commandNames.push_back("PRIVMSG");
 	_commandNames.push_back("TOPIC");
 	_commandNames.push_back("USER");
 	_commandNames.push_back("MODE");
 	_commandNames.push_back("DISPLAY");
-	_commandNames.push_back("PASS");
-	_commandNames.push_back("KICK");
 	_commandPointers[0] = &IrcCommand::INVITE;
 	_commandPointers[1] = &IrcCommand::JOIN;
 	_commandPointers[2] = &IrcCommand::NICK;
 	_commandPointers[3] = &IrcCommand::PART;
-	_commandPointers[4] = &IrcCommand::PING;
+	_commandPointers[4] = &IrcCommand::PONG;
 	_commandPointers[5] = &IrcCommand::PRIVMSG;
 	_commandPointers[6] = &IrcCommand::TOPIC;
 	_commandPointers[7] = &IrcCommand::USER;
 	_commandPointers[8] = &IrcCommand::MODE;
 	_commandPointers[9] = &IrcCommand::DISPLAY;
-	_commandPointers[10] = &IrcCommand::PASS;
-	_commandPointers[11] = &IrcCommand::KICK;
+
 }
 IrcCommand::IrcCommand(IrcDB *db, int clientFd): _db(db), _clientFd(clientFd) {
 	_commandList["INVITE"] = &IrcCommand::INVITE;
 	_commandList["JOIN"] = &IrcCommand::JOIN;
 	_commandList["NICK"] = &IrcCommand::NICK;
 	_commandList["PART"] = &IrcCommand::PART;
-	_commandList["PONG"] = &IrcCommand::PING;
+	_commandList["PONG"] = &IrcCommand::PONG;
 	_commandList["PRIVMSG"] = &IrcCommand::PRIVMSG;
 	_commandList["TOPIC"] = &IrcCommand::TOPIC;
 	_commandList["USER"] = &IrcCommand::USER;
 	_commandList["MODE"] = &IrcCommand::MODE;
 	_commandList["DISPLAY"] = &IrcCommand::DISPLAY;
-	_commandList["PASS"] = &IrcCommand::PASS;
-	_commandList["KICK"] = &IrcCommand::KICK;	
 	_commandNames.push_back("INVITE");
 	_commandNames.push_back("JOIN");
 	_commandNames.push_back("NICK");
 	_commandNames.push_back("PART");
-	_commandNames.push_back("PING");
+	_commandNames.push_back("PONG");
 	_commandNames.push_back("PRIVMSG");
 	_commandNames.push_back("TOPIC");
 	_commandNames.push_back("USER");
 	_commandNames.push_back("MODE");
 	_commandNames.push_back("DISPLAY");
-	_commandNames.push_back("PASS");
-	_commandNames.push_back("KICK");
 	_commandPointers[0] = &IrcCommand::INVITE;
 	_commandPointers[1] = &IrcCommand::JOIN;
 	_commandPointers[2] = &IrcCommand::NICK;
 	_commandPointers[3] = &IrcCommand::PART;
-	_commandPointers[4] = &IrcCommand::PING;
+	_commandPointers[4] = &IrcCommand::PONG;
 	_commandPointers[5] = &IrcCommand::PRIVMSG;
 	_commandPointers[6] = &IrcCommand::TOPIC;
 	_commandPointers[7] = &IrcCommand::USER;
 	_commandPointers[8] = &IrcCommand::MODE;
 	_commandPointers[9] = &IrcCommand::DISPLAY;
-	_commandPointers[10] = &IrcCommand::PASS;
-	_commandPointers[11] = &IrcCommand::KICK;
 }
 IrcCommand::~IrcCommand(){}
 
@@ -120,14 +109,9 @@ void IrcCommand::parsing(std::string message){
 		for (end = it->find_first_of(delim); end != -1; end = it->find_first_of(delim)){
 			_args.push_back(it->substr(0, end));
 			it->erase(0, end + 1);
-			if (i == 0){
+			if (i == 0)
 				_command = _args[0];
-				if (_command == "JOIN" || _command == "KICK")
-					delim = " \t\v\f";
-			}
 			if ((!_command.compare("PRIVMSG") || !_command.compare("TOPIC")) && i > 0)
-				break;
-			if ((!_command.compare("KICK")) && i == 2)
 				break;
 			i++;
 		}
@@ -176,6 +160,4 @@ const char* IrcCommand::ERR_CHANOPRIVSNEEDED::what() const throw() { return "ERR
 //MODE
 const char* IrcCommand::ERR_UNKNOWNMODE::what() const throw() { return "ERR_UNKNOWNMODE"; }
 
-//KICK
-const char* IrcCommand::ERR_NOT_OPERATOR::what() const throw() { return "ERR_NOT_OPERATOR"; }
 
