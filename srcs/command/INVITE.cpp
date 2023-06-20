6001cd8e7da654f89da7734956b12c9a1f870c82
Jihye Wee <jwee@c5r2s7.42seoul.kr>
2023-06-20 21:26:37 +0900

    invite error

--- srcs/command/INVITE.cpp
+++ srcs/command/INVITE.cpp
@@ -3,23 +3,31 @@
 #include "../../include/IrcClient.hpp"
 
 void IrcCommand::INVITE(){
-	if (_args.size() != 2)
-		throw ERR_NEEDMOREPARAMS();
-
-	std::cout << "_args list : " << "<" << _args[0] << "> " << "<" << _args[1] << ">" << std::endl;
 	IrcClient *host = _db->findClientByFd(_clientFd);
+
+	if (_args.size() != 2){
+		host->addBackBuffer("433 <" + _command + "> ");
+		throw ERR_NEEDMOREPARAMS();
+	}
+
 	IrcClient *target = _db->findClientByName(_args[0]);
 	IrcChannel *channel = _db->findChannel(_args[1]);
 
 	// 채널 limit 검사
-	if ((channel->getGrant() & M_LIMIT) && (channel->getLimit() <= channel->getUser().size()))
-		host->addBackCarriageBuffer("The channel is full");
+	if ((channel->getGrant() & M_LIMIT) && (channel->getLimit() <= channel->getUser().size())){
+		host->addBackBuffer("471 <" + channel->getName() + "> ");
+		throw ERR_CHANNELISFULL();
+	}
 	//권한 검사
-	if ((channel->getGrant() & M_INVITE) && !channel->isOperator(_clientFd))
-		throw ERR_NOPRIVILEGES();
+	if ((channel->getGrant() & M_INVITE) && !channel->isOperator(_clientFd)){
+		host->addBackBuffer("482 <" + channel->getName() + "> ");
+		throw ERR_CHANOPRIVSNEEDED();
+	}
 	//이미 존재하는 유저인지 검사
-	if (channel->isJoinedUser(target->getFd()))
+	if (channel->isJoinedUser(target->getFd())){
+		host->addBackBuffer("443 <" + target->getNickname() + "> " + "<" + channel->getName() + "> ");
 		throw ERR_USERONCHANNEL();
+	}
 	channel->addUser(target->getFd());
 	
 	host->addBackCarriageBuffer(":" + host->getNickname() + " INVITE " + target->getNickname() + " " + channel->getName());
