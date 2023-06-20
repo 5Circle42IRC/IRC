026b4c65cfc8e53d13baaf10f07fccb6dd60037f
ysungwon <hwoarang09@naver.com>
2023-06-20 16:21:37 +0900

    edit ping

--- srcs/command/PONG.cpp
+++ srcs/command/PONG.cpp
@@ -2,6 +2,7 @@
 #include "../../include/IrcClient.hpp"
 
 void IrcCommand::PING(){
+    std::cout << "IN PING" << std::endl;
     IrcClient* client = _db->findClientByFd(_clientFd);
     client->addBackBuffer(client->getNickname() + " PONG" + "\r\n");
 }
